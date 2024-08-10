eb6db40a3240c3207a6a8644aa6a8041789977d6
jyrnan <yoeking@gmail.com>
2024-08-10 10:24:03 +0800

    Nothing to commit

--- Sources/FileOnNewFeature.swift
+++ Sources/FileOnNewFeature.swift
@@ -1,3 +1,8 @@
 // this is the file on newFeature to test pull request
 
 // add more commit
+
+struct Person {
+    var name: String
+    var age: Int
+}
--- Sources/main.swift
+++ Sources/main.swift
@@ -1,13 +1,31 @@
+import Foundation
+
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
+// Function to print numbers from 1 to 100
 func sum100() {
+    // Iterate through numbers 1 to 100 using a range
     (1...100).forEach{
-
-    print($0)
+        // Print each number
+        print($0)
     }
 }
 
-print("Hello, world!")
-sum100()
-sum100()
+// Print a greeting message
+let person: Person = Person(name: "John", age: 30)
+print("Hello, world!", person)
+// Function to validate an email address
+func isValidEmail(_ email: String) -> Bool {
+    let emailRegEx: String = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+    let emailPred: NSPredicate = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+    return emailPred.evaluate(with: email)
+}
+
+// Example usage:
+let testEmail: String = "example@email.com"
+if isValidEmail(testEmail) {
+    print("\(testEmail) is a valid email address.")
+} else {
+    print("\(testEmail) is not a valid email address.")
+}
